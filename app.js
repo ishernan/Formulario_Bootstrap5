5893d863ac24ec0129972460286b11d016b21239
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-17 09:45:07 +0200

    spinner

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
-const formulario = document.querySelector('#formulario');
+const formulario  = document.querySelector('#formulario');
+const btnEnviar   = document.querySelector('#btnEnviar');
+const btnCargando = document.querySelector('#btnCargando');
+
 
 formulario.addEventListener('submit', (e)=> {
   e.preventDefault();
@@ -9,6 +12,14 @@ formulario.addEventListener('submit', (e)=> {
   console.log('campo Email', datos.get('emailCampo'))
   console.log('campo Password', datos.get('passCampo'))
   console.log('campo CheckBox', datos.get('checkCampo'))
+
+  btnEnviar.classList.add("d-none")
+  btnCargando.classList.remove('d-none')
+
+  setTimeout(()=> {
+   btnEnviar.classList.remove("d-none")
+   btnCargando.classList.add('d-none')
+  }, 3000); 
 
   formulario.reset();//pour remettre a zero le formulaire  
   
